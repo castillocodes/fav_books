e494d40cd74f9e32d77344aa72c6a11201524f8d
castillocodes <janeirosergio@gmail.com>
2021-09-15 14:29:30 -0500

    Login & Registration

--- fav_books_app/views.py
+++ fav_books_app/views.py
@@ -1,6 +1,39 @@
-from django.http.response import HttpResponseRedirect
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .models import *
+import bcrypt
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Cha ching!")
+    return render(request, "index.html")
+
+def register(request):
+    errors = User.objects.register_validator(request.POST)
+
+    if len(errors):
+        for key, value in errors.items():
+            messages.error(request, value)
+        return redirect('/')
+    else:
+        user = User.objects.create(
+            first_name = request.POST['first_name'],
+            last_name = request.POST['last_name'],
+            email = request.POST['email'],
+            password = bcrypt.haspw(request.POST['password'].encode(), bcrypt.gensalt()).decode()
+        )
+        request.session['user_id'] = user.id
+        request.session['greeting'] = user.first_name
+        return redirect('/books')
+
+def login(request):
+    errors = User.objects.login_validator(request.POST)
+
+    if len(errors):
+        for key, value in errors.items():
+            messages.error(request, value)
+        return redirect('/')
+    else:
+        user = User.objects.get(email=request.POST['login_email'])
+        request.session['user_id'] = user.id
+        request.session['greeting'] = user.first_name
+        return redirect('/books')
