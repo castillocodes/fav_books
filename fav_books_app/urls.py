7dbd73a43dfd99a3e1a8bcfe0c56d57139d312a9
castillocodes <janeirosergio@gmail.com>
2021-09-16 09:37:16 -0500

    admin panel

--- fav_books_app/urls.py
+++ fav_books_app/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
+from django.conf.urls import url
+from django.contrib import admin
 from . import views
 
 urlpatterns = [
+    url(r'^admin/', admin.site.urls),
     path('', views.index),
     path('register', views.register),
     path('login', views.login),
     path('books', views.show_all),
-    path("books/create", views.create_book),
-    path("books/<int:book_id>", views.show_one),
+    path('books/create', views.create_book),
+    path('books/<int:book_id>', views.show_one),
 ]
